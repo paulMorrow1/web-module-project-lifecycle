354b4cbc9107c1a6e3b1859cd2f46fac0e90392f
Paul Morrow <pmorow93@gmail.com>
2022-05-11 09:56:26 -0500

    Imported Axios

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
   render() {
-    return null
+    return null;
   }
 }
